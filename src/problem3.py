bbf0fbe48cef1667168c3fbf2280354190518ed9
wenningr <wenningr@rose-hulman.edu>
2018-12-09 16:05:32 -0500

    DONE

--- src/problem3.py
+++ src/problem3.py
@@ -231,7 +231,7 @@ def problem3b(m, point1):
         :type point1: rg.Point
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
